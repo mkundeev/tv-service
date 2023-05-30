929396d5577d654e8a0441f9a321fee9a98a61fe
Maksym Kundeev <mkundeev@gmail.com>
2023-05-30 13:11:20 +0300

    Update AboutPage.tsx

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -1,10 +1,5 @@
 import { useRouter } from "next/router";
-import { BsFillHandThumbsUpFill } from "react-icons/bs";
-import { AiFillSafetyCertificate, AiFillHome } from "react-icons/ai";
-import { GiStamper } from "react-icons/gi";
-import { FaMapMarkedAlt } from "react-icons/fa";
 import * as Styled from "./styles/AboutPage.styles";
-import { COLORS } from "@/theme";
 import AboutList from "./AboutList";
 
 export default function AboutPage() {
