bec52ef580ff3d0e0b21137ebbbab4c65c3eefe0
Maksym Kundeev <mkundeev@gmail.com>
2023-06-01 17:40:25 +0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -48,7 +48,7 @@ export default class MyDocument extends Document {
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="../../public/favicon.png"
+            href="/favicon.png"
           ></link>
         </Head>
         <body>
